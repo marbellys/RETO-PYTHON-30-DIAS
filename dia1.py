6fc3e118e93b2919419010b7dfd3fbd2d18647cc
marbellys campos casa <marbellys.campos@gmail.com>
2022-01-10 14:18:38 -0400

    dia1

--- dia1.py
+++ dia1.py
@@ -1,11 +1,11 @@
 #DIA 1 - OPERADORES ARITMETICOS Y TIPOS DE DATOS
 
-print(2 + 6) #suma
+print(2 + 6)  #suma
 print(10 - 2) #resta
-print(3 * 4) #multiplicacion
+print(3 * 4)  #multiplicacion
 print(10 / 3) #division
 print(2 ** 4) #potencia
-print(9 % 2) #modulo
+print(9 % 2)  #modulo
 print(10 // 3) #Floor division operator
 
 #TIPOS DE DATOS
