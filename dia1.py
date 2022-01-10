148edaae6d5559febf688d2297bc4f47419c1ef7
marbellys campos casa <marbellys.campos@gmail.com>
2022-01-10 14:16:52 -0400

    dia1 y dia 2

--- dia1.py
+++ dia1.py
@@ -1,1 +1,21 @@
-printr(2+6)
+#DIA 1 - OPERADORES ARITMETICOS Y TIPOS DE DATOS
+
+print(2 + 6) #suma
+print(10 - 2) #resta
+print(3 * 4) #multiplicacion
+print(10 / 3) #division
+print(2 ** 4) #potencia
+print(9 % 2) #modulo
+print(10 // 3) #Floor division operator
+
+#TIPOS DE DATOS
+print(type(4)) #int
+print(type(4.5)) #Float
+print(type(2+4j)) #Complejo
+print(type("Buenos dias Chamos")) #String
+print(type('Buenos dias Chamos')) #String
+print(type('B')) #String
+print(type(['beisball','natacion','basquet'])) #list
+print(type({'nombre':'pepe','edad':22,'gustos':['dormir','comer']}))#Diccionario
+print(type(('A','B','X','Y'))) #Tupla
+      
